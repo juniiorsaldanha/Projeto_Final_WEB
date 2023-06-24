23e7d8831f42937d58120dc7ab98e433aa1d7338
juniiorsaldanha <gsaldanhajunior@outlook.com>
2023-06-24 19:55:02 -0300

    Criando enquente

--- src/App.jsx
+++ src/App.jsx
@@ -43,23 +43,25 @@ export function App() {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
-        <Sidebar />
-        <main>
-          {posts.map(post => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            )
-          })}
-        </main>
+        <Sidebar /> 
+          <main>
+            {posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
+          </main>
+        </div>
+        <div className={styles.wrapper}>
+          <Poll />
+        </div>
       </div>
-    </div>
   )
 }
 
